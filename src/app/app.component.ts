850ea0ce5aa5f871b1a66f60325e80ee36bfd9bc
= <edvin.sakalauskas@gmail.com>
2023-05-31 21:49:36 +0100

    angular to do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from "./item";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Angular To do application';
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "study", isComplete: true },
+    { description: "eat", isComplete: false },
+    { description: "exercise", isComplete: false },
+    { description: "contemplate", isComplete: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.isComplete : !item.isComplete
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      isComplete: false
+    });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+
 }
+
